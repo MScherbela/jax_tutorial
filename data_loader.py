b4cc02a524ba5b581850a134c838e0475b2b9acf
mscherbela <michael.scherbela@gmail.com>
2022-06-29 17:38:11 +0200

    Fleshed out lesson 2 and lesson 3

---
+++ data_loader.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def load_qm7(n_train=6500):
+    data = np.load("data/qm7.npz")
+    n_samples = len(data['X'])
+    ind = np.random.permutation(n_samples)
+    X_train, X_test = np.split(data['X'][ind], [n_train])
+    y_train, y_test = np.split(data['T'].flatten()[ind], [n_train])
+    return X_train, y_train, X_test, y_test
+
+
+def make_batches(x, y, n_batches):
+    n_samples = len(x)
+    assert n_samples % n_batches == 0
+    batch_size = n_samples // n_batches
+    indices = np.random.permutation(len(x))
+    x = x[indices]
+    y = y[indices]
+    for n in range(n_batches):
+        yield x[n*batch_size:(n+1)*batch_size], y[n*batch_size:(n+1)*batch_size]
