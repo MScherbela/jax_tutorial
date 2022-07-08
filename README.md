3e896d91c22f6238666e7a4bb87adef0ca2a3ffd
mscherbela <michael.scherbela@gmail.com>
2022-07-08 12:04:43 +0200

    Notebooks are now self-contained; Added sklearn example

---
+++ README.md
@@ -0,0 +1,14 @@
+Basics of Machine Learning
+==========================
+
+These are the code examples for the tutorial on day 1 of the 2022 summer school *Machine Learning for Materials Hard and Soft*.
+
+In this session, you will learn the following:
+
+1. How to use scikit-learn to apply simple machine learning algorithmis, using very little code 
+2. What are the very basics of JAX, a machine learning library by Google
+3. How to implement a simple neural network from scratch using JAX
+4. How to improve your model and training using JAX helper libraries
+
+All code for these sessions is contained in standalone jupyter notebooks, which can run on google colab.
+
--- data_loader.py
+++ data_loader.py
@@ -1,18 +1,13 @@
 import numpy as np
+import pandas as pd
 
 def load_qm7(n_train=6500):
-    data = np.load("data/qm7.npz")
-    n_samples = len(data['X'])
-    ind = np.random.permutation(n_samples)
-    X_train, X_test = np.split(data['X'][ind], [n_train])
-    y_train, y_test = np.split(data['T'].flatten()[ind], [n_train])
+    df = pd.read_csv("https://raw.githubusercontent.com/MScherbela/jax_tutorial/master/data/qm7.csv")
+    X = df.values[:, :-1]
+    Y = df.values[:, -1]
+    ind = np.random.permutation(len(X))
+    X_train, X_test = np.split(X[ind], [n_train])
+    y_train, y_test = np.split(Y[ind], [n_train])
     return X_train, y_train, X_test, y_test
 
 
-def make_batches(x, y, n_batches):
-    batch_size = len(x) // n_batches
-    indices = np.random.permutation(len(x))
-    x = x[indices]
-    y = y[indices]
-    for n in range(n_batches):
-        yield x[n*batch_size:(n+1)*batch_size], y[n*batch_size:(n+1)*batch_size]
